7c5cb852cad474ba81a5ff854d51c3e39f28f7dc
unknown <mahlasadat.zohourparvaz@part.local>
2025-11-26 11:00:40 +0330

    feat: publish as NPM setup for package

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import DatePicker from './components/ui/DatePicker.vue';
+import DatepickerInput from './components/ui/DatepickerInput.vue';
+import { useDatePicker } from './composables/useDatePicker';
+
+export { DatePicker, DatepickerInput, useDatePicker };
+
+export default DatePicker;
